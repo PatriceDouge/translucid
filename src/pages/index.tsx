f7f4da54a8cec942dabd2bbd7f81c3c71031d1b1
Patrice Douge <patricedouge@gmail.com>
2023-10-15 12:12:06 -0400

    saving work - added about us section, FE cleanup, contact us form added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,18 @@
-import { useState } from 'react'
+import { Fragment, useRef, useState } from 'react'
 import Image from 'next/image'
-import { Dialog } from '@headlessui/react'
+import { Dialog, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { UsersIcon, GlobeAmericasIcon, PencilSquareIcon, MicrophoneIcon } from '@heroicons/react/24/solid'
+import { UsersIcon, GlobeAmericasIcon, PencilSquareIcon, MicrophoneIcon, PhotoIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link';
 import WomenTalking from '@/images/women_talking.jpg';
-
+import ContactForm from '@/components/ContactForm/contact'
 
 const navigation = [
-  { name: 'About us', href: 'about' },
   { name: 'Client Solutions', href: 'client-solutions' },
+  { name: 'About us', href: 'about' },
   { name: 'Career Opportunities', href: 'career' },
 ]
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
 
 const features = [
   {
@@ -45,16 +41,44 @@ const features = [
   },
 ]
 
+const aboutUs = [
+  {
+    name: 'Vision',
+    description:
+      'Empowering You and the Haitian Creole speakers with seamless language access, fostering effective communication without linguistic barriers.',
+  },
+  {
+    name: 'Mission',
+    description:
+      'Providing tailored Haitian Creole language solutions for individuals, businesses, and organizations across diverse sectors, including Healthcare, Legal Justice, and Education.',
+  },
+  {
+    name: 'Purpose',
+    description:
+      'Bridging language gaps within the Haitian Creole community and empowering language access for clients by unleashing the power of communication.',
+  },
+]
+
+const footer = {
+  main: [
+    { name: 'About', href: '#' },
+    { name: 'Client Solutions', href: '#' },
+    { name: 'Career Opportunities', href: '#' },
+  ]
+}
+
 export default function Index() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
 
 
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="flex items-center justify-between max-w-7xl mx-auto p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="text-lg font-semibold leading-6 text-gray-900">
+            <a href="#" className="text-4xl font-bold leading-6 text-gray-900">
                 Translucid
             </a>
           </div>
@@ -144,6 +168,7 @@ export default function Index() {
                 <a
                   href="#"
                   className="rounded-md bg-blue-950 px-7 py-3 text-lg font-semibold text-white shadow-sm hover:bg-blue-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                  onClick={() => setOpen(true)}
                 >
                   Get in touch
                 </a>
@@ -157,52 +182,90 @@ export default function Index() {
         </div>
       </div>
 
-      <div className="mx-auto max-w-7xl px-6 py-10 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              About us
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              At Translucid Languages, our client-centric approach involves understanding your distinct 
-              requirements, objectives, and brand identity to provide tailored solutions aligned with your vision. 
-              Our commitment to meticulous care and confidentiality ensures your projects are in trusted hands.
-            </p>
-          </div>
-          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              {features.map((feature) => (
-                <div key={feature.name} className="relative pl-16">
-                  <dt className="text-base font-semibold leading-7 text-gray-900">
-                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-950">
-                      <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                    </div>
-                    {feature.name}
-                  </dt>
-                  <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+      <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <ContactForm></ContactForm>
+                <div className="mt-5">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md bg-blue-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-900"
+                    onClick={() => setOpen(false)}
+                  >
+                  {`Submit`}
+                  </button>
                 </div>
-              ))}
-            </dl>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
+      </Dialog>
+    </Transition.Root>
 
-      <div className="bg-slate-100">
-        <div className="mx-auto max-w-7xl px-6 py-10 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Client Solutions
-            </h1>
-          </div>
-          <div className="mx-auto mt-16 max-w-2xl sm:mt-12 lg:mt-18 lg:max-w-4xl">
-            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              {features.map((feature) => (
-                <div key={feature.name} className="relative pl-16">
+      <div className="mx-auto max-w-7xl px-6 py-10 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Client Solutions
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            At Translucid Languages, our client-centric approach involves understanding your distinct 
+            requirements, objectives, and brand identity to provide tailored solutions aligned with your vision. 
+            Our commitment to meticulous care and confidentiality ensures your projects are in trusted hands.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-950">
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+
+      <div className="bg-slate-100 mt-10 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              About Us
+            </h2>
+            <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-16">
+              {aboutUs.map((items) => (
+                <div key={items.name}>
                   <dt className="text-base font-semibold leading-7 text-gray-900">
-                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-950">
-                      <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                    </div>
-                    {feature.name}
+                    {items.name}
                   </dt>
-                  <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                  <dd className="mt-1 text-base leading-7 text-gray-600">{items.description}</dd>
                 </div>
               ))}
             </dl>
@@ -212,13 +275,11 @@ export default function Index() {
 
 
       <div className="mx-auto max-w-2xl px-4 pt-26 sm:pt-32 lg:pt-36 text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Contact us
-        </h1>
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          We're Hiring!
+        </h2>
         <p className="my-6 text-lg leading-8 text-gray-600">
-          Contact us today to explore how Translucid Languages can assist you with
-          your Haitian Creole language needs. Let&#39;s work together to create a world
-          where communication knows no bounds.
+          Join our team! We're actively seeking talented translators and interpreters to shape language excellence with us.
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-12">
@@ -247,20 +308,6 @@ export default function Index() {
                 name="last-name"
                 id="last-name"
                 autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="translucid" className="block text-sm font-semibold leading-6 text-gray-900">
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="translucid"
-                id="translucid"
-                autoComplete="organization"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
               />
             </div>
@@ -312,17 +359,24 @@ export default function Index() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-              Message
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Resume
             </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
-                defaultValue={''}
-              />
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">PDF, DOC, DOCX, TXT, RTF</p>
+              </div>
             </div>
           </div>
         </div>
@@ -335,6 +389,14 @@ export default function Index() {
           </button>
         </div>
       </form>
+
+      <footer className="bg-white mt-8">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          &copy; 2023 Translucid, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
     </div>
   )
 }

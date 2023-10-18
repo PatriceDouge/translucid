0571af627b327af198b0df8144740535cfffae05
Patrice Douge <patricedouge@gmail.com>
2023-10-17 22:59:59 -0400

    contact form updates

--- src/components/ContactForm/contact.tsx
+++ src/components/ContactForm/contact.tsx
@@ -53,18 +53,51 @@ export default function Contact() {
                 </div>
             </div>
             <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                Company
+                </label>
+                <div className="mt-2.5">
+                <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    autoComplete="organization"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
+                />
+                </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                What industry in your company in?
+              </label>
+              <div className="mt-2">
+                <select
+                  id="industry"
+                  name="industry"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+                >
+                  <option>Healthcare</option>
+                  <option>Legal</option>
+                  <option>Insurance</option>
+                  <option>Financial Services</option>
+                  <option>Government</option>
+                  <option>Education</option>
+                  <option>Personal</option>
+                  <option>Other</option>
+                </select>
+              </div>
+            </div>
+            <div className="sm:col-span-2">
                 <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
-                Phone number
+                    Phone number
                 </label>
                 <div className="relative mt-2.5">
                 <div className="absolute inset-y-0 left-0 flex items-center">
-                    <label htmlFor="country" className="sr-only">
-                    Country
-                    </label>
+                    <label htmlFor="country" className="sr-only">Country</label>
                     <select
-                    id="country"
-                    name="country"
-                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
+                        id="country"
+                        name="country"
+                        className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
                     >
                     <option>US</option>
                     <option>CA</option>

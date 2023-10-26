c7052301d66dcf5b8e22979fb959a8aa3f85119d
Patrice Douge <patricedouge@gmail.com>
2023-10-26 09:08:07 -0400

    fix mobile menu not closing after clicking on link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,13 +150,18 @@ export default function Index() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
+                    <Link 
+                      key={item.name} 
+                      href={item.href} 
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollToSection(item.scroll);
+                        setMobileMenuOpen(false);
+                      }}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
